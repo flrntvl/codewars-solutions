d3a2849806d160b76d56346a107af4cbe0d13313
Florent <267955681+flrntvl@users.noreply.github.com>
2026-03-14 19:09:32 +0100

    ✨ Add Ruby solution for 8-kyu/even_or_odd

---
+++ ruby/8-kyu/even_or_odd/main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Challenge : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/ruby
+
+def even_or_odd(number)
+  number.odd? ? 'Odd' : 'Even'
+end
---
+++ ruby/8-kyu/even_or_odd/spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'main'
+
+describe '8-kyu/even_or_odd' do
+  it 'even_or_odd(1) should return "Odd"' do
+    expect(even_or_odd(1)).to eq('Odd')
+  end
+  it 'even_or_odd(2) should return "Even"' do
+    expect(even_or_odd(2)).to eq('Even')
+  end
+  it 'even_or_odd(-1) should return "Odd"' do
+    expect(even_or_odd(-1)).to eq('Odd')
+  end
+  it 'even_or_odd(-2) should return "Even"' do
+    expect(even_or_odd(-2)).to eq('Even')
+  end
+  it 'even_or_odd(0) should return "Even"' do
+    expect(even_or_odd(0)).to eq('Even')
+  end
+end
