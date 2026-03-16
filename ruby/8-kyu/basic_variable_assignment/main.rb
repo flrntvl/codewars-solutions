54c162f0cf192a389a96431767aa0a44ac6e4ffe
Florent <267955681+flrntvl@users.noreply.github.com>
2026-03-16 19:55:01 +0100

    ✨ Add Ruby solution for 8-kyu/basic_variable_assignment

---
+++ ruby/8-kyu/basic_variable_assignment/main.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/ruby
+
+# Exposed as a method to keep the RSpec test simple and reliable.
+def name
+  'codewa.rs'
+end
+
+# Alternative solution using string concatenation:
+# a = 'code'
+# b = 'wa.rs'
+# name = a + b
+
+# Alternative solution using a direct assignment:
+# name = 'codewa.rs'
---
+++ ruby/8-kyu/basic_variable_assignment/spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'main'
+
+describe '8-kyu/basic_variable_assignement' do
+  it 'name should be equal to "codewa.rs"' do
+    expect(name).to eq('codewa.rs')
+  end
+end
